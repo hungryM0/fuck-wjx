458f96f11b6f7a704149a06bf93662be7112bd03
HUNGRY_M0 <25040072@muc.edu.cn>
2025-12-06 23:12:51 +0800

    自动提交 周六-2025-12 23:12

---
+++ version.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+版本号及相关常量配置
+
+修改版本号时只需修改此文件中的 __VERSION__ 变量即可
+"""
+
+# 版本号
+__VERSION__ = "1.0-pre3"
+
+# GitHub 仓库配置
+GITHUB_OWNER = "hungryM0"
+GITHUB_REPO = "fuck-wjx"
+
+# 以下常量基于上述配置自动生成，一般无需修改
+GITHUB_API_URL = f"https://api.github.com/repos/{GITHUB_OWNER}/{GITHUB_REPO}/releases/latest"
+ISSUE_FEEDBACK_URL = f"https://github.com/{GITHUB_OWNER}/{GITHUB_REPO}/issues/new"
