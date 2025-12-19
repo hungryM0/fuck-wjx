c8b0587931fb2da496f1cf2b8168c96ff3cbba54
HUNGRY_M0 <25040072@muc.edu.cn>
2025-12-19 13:29:40 +0800

    AutoCommit 周五-2025-12 13:29

--- wjx/__init__.py
+++ wjx/__init__.py
@@ -1,5 +1,23 @@
 """Support modules for the WJX automation package."""
 
+# 导入子模块使其可以通过 wjx.* 访问
+from wjx import (
+    boot,
+    browser_driver,
+    config,
+    engine,
+    full_simulation_mode,
+    full_simulation_ui,
+    gui,
+    load_save,
+    log_utils,
+    random_ip,
+    registry_manager,
+    runtime,
+    updater,
+    version,
+)
+
 __all__ = [
     "config",
     "full_simulation_mode",
